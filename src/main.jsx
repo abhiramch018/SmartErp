6703c99bb39278e0952f0a68b0d6a40e3bebf567
abhiramch018 <abhiramch018@gmail.com>
2025-11-04 15:12:16 +0530

    Erp

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import App from './App.jsx';
-import Login from './Login.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {BrowserRouter, Router, Routes, Route} from 'react-router-dom'
+import App from './App.jsx'
+import Login from './Login.jsx'
+import Dashboard from './Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/app' element={<App />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
